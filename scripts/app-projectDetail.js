6a56bec415788a52241f85c3eb663cff360fa1d8
mazong1123 <mazong1123@gmail.com>
2015-10-05 19:22:35 +0800

    Added project detail page.

---
+++ scripts/app-projectDetail.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var rachetPageManager = new window.RATCHET.Class.PageManager();
+    rachetPageManager.ready(function () {
+        var projectId = 1;
+
+        var selectedLi = document.querySelector('#projectDetailList').querySelector('li["data-project-id"=' + projectId + ']');
+        var selectedProjectTitle = selectedLi.querySelector('h1').innerHtml;
+        var selectedProjectDescription = selectedLi.querySelector('p').innerHtml;
+    });
+})();
